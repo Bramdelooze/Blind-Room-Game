d764c6b9dfb6af4d1e0a5fcb34dbd9b58ac9e53c
Bramdelooze <39378407+Bramdelooze@users.noreply.github.com>
2018-06-19 14:07:15 +0200

    Added a phone call function

--- Blind Room Game 5.0/Assets/Scripts/InteractOneTimeSound.cs
+++ Blind Room Game 5.0/Assets/Scripts/InteractOneTimeSound.cs
@@ -39,6 +39,7 @@ public class InteractOneTimeSound : ObjectInteraction {
             sound.Stop();
             if(gameObject.tag == "Phone")
             {
+                interactable = false;
                 Instantiate(conversationPhone, gameObject.transform);
             }
         }
